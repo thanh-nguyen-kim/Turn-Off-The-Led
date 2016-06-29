470a41a5fed60e057360db68020f105b89575106
Nguyễn Kim Thành <thanhnk@sunnet.vn>
2016-06-29 17:09:10 +0700

    init

---
+++ Assets/Scripts/Element.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Element : MonoBehaviour
+{
+    public bool state;
+    public GameObject[] neighbors;
+
+    public void OnMouseDown()
+    {
+        ChangeState();
+        foreach (GameObject neighbor in neighbors)
+            neighbor.GetComponent<Element>().ChangeState();
+    }
+
+    public void ChangeState()
+    {
+        state = !state;
+        if (state)
+            GetComponentInChildren<Animator>().Play("On");
+        else
+            GetComponentInChildren<Animator>().Play("Off");
+    }
+
+}
